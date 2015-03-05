e0b890ef12b32ad953ba1f9a9c9ff0d194965c41
Boom Tantikarun <bonedz@gmail.com>
2015-03-05 10:55:15 +0700

    no message

--- app/views/home/market.blade.php
+++ app/views/home/market.blade.php
@@ -5,7 +5,15 @@
 <div id="mainmenu-body">
 	<div id="mainmenu-header">MARKET OVERVIEW</div>
 	<div class="triangle-down"></div>
+	<div id="mk-menu">
+		<ul>
+			<li>SET</li>
+			<li>SET50</li>
+			<li>SET100</li>
+			<li>SETHD</li>
+			<li>MAI</li>
+		</ul>
+	</div>
 </div>
-
 
 @stop
